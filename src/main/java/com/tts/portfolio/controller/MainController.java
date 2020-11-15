5cf7859302f2893481d9e3fd30bd7fbd16dd7a1f
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-15 17:23:01 -0600

    controller to handle comment variables

--- src/main/java/com/tts/portfolio/controller/MainController.java
+++ src/main/java/com/tts/portfolio/controller/MainController.java
@@ -1,24 +1,78 @@
 package com.tts.portfolio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tts.portfolio.model.Comments;
+import com.tts.portfolio.repository.CommentsRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
-    
 
-    @GetMapping("/")
+
+    @Autowired
+    private CommentsRepository commentsRepository;
+
+    private List<Comments> posts = new ArrayList<>();
+
+
+
+    @GetMapping(value = "/")
+    public String greeting() {
+
+        return "greeting";
+    }
+
+    @GetMapping(value = "/index")
     public String index() {
 
         return "index";
     }
 
-    @GetMapping("/design")
+    @GetMapping(value = "/design")
     public String designPage() {
 
         return "design";
     }
 
+    @GetMapping(value = "/codingprojects")
+    public String softwareDevPage() {
 
+        return "codingprojects";
+    }
+
+
+
+    @GetMapping(value = "/comments")
+    public String commentsPage(Comments comments, Model model) {
+
+        for (Comments postFromDB : commentsRepository.findAll()) {
+            posts.add(postFromDB);
+        }
+
+        model.addAttribute("posts", posts);
+
+        return "comments";
+    } 
+
+       
+
+    @PostMapping(value = "/comments")
+    public String addNewComment(Comments comments, Model model) {
+
+        commentsRepository.save(comments);
+
+        model.addAttribute("title", comments.getTitle());
+        model.addAttribute("author", comments.getAuthor());
+        model.addAttribute("entry", comments.getEntry());
+
+        return "comments";
+    }
 
 }
