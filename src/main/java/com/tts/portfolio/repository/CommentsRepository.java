ec2059e9e9945e36f327ca99ae45246ff4e53683
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-15 17:23:37 -0600

    comment repo

---
+++ src/main/java/com/tts/portfolio/repository/CommentsRepository.java
@@ -0,0 +1,9 @@
+package com.tts.portfolio.repository;
+
+import com.tts.portfolio.model.Comments;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentsRepository extends CrudRepository<Comments, Long>{
+        
+} 
